99b83e208965a9ce7f84762fc3b2b02af5281e0a
Connor P. Jackson <cpjackson@berkeley.edu>
2020-03-30 16:55:54 -0700

    Add pandoc to the path, don't print render status

--- render_file.R
+++ render_file.R
@@ -2,7 +2,9 @@
 # A custom preprocessor for Marked 2 to call rmarkdown::render() on the source document,
 # then cancel the processor and have Marked resume processing.
 
-rmarkdown::render(Sys.getenv("MARKED_PATH"))
+Sys.setenv(RSTUDIO_PANDOC = "/Applications/RStudio.app/Contents/MacOS/pandoc/")
+
+rmarkdown::render(Sys.getenv("MARKED_PATH"), quiet = TRUE)
 #   - specify output format explicitly?
 #   - specify output location explicitly?
 cat("NOCUSTOM")  # to return to the main processor
