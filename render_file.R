12f06bbbf12547dd76ef7fd4b58a7a405ac6a9d1
Connor P. Jackson <cpjackson@berkeley.edu>
2020-03-30 17:21:00 -0700

    Improve comments

--- render_file.R
+++ render_file.R
@@ -2,14 +2,16 @@
 # A custom preprocessor for Marked 2 to call rmarkdown::render() on the source document,
 # then cancel the processor and have Marked resume processing.
 
+## First get the environment set up so we can call render()
+# Work around a bug in render() that doesn't remove .tex file when the working directory
+# is not where the file is
+setwd(Sys.getenv("MARKED_ORIGIN"))
 
-# get the environment set up so we can call render()
-setwd(Sys.getenv("MARKED_ORIGIN"))  # Workaround a bug that doesn't remove the .tex file
-# Add pandoc and LaTeX to PATH so render() can find them
+# Add pandoc and LaTeX to PATH so render() can find them.
+# Note these hardcoded paths might need to be changed for different systems
 Sys.setenv(RSTUDIO_PANDOC = "/Applications/RStudio.app/Contents/MacOS/pandoc/")
 Sys.setenv(PATH = paste(Sys.getenv("PATH"), "/Library/TeX/texbin/", sep = ":"))
 
 rmarkdown::render(Sys.getenv("MARKED_PATH"), quiet = TRUE)
-#   - specify output format explicitly?
-#   - specify output location explicitly?
+
 cat("NOCUSTOM")  # to return to the main processor
